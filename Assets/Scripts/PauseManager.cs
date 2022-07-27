7477cd34da35c6b28c7cf15532dd4fab691df1e8
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-07-27 17:09:44 +0300

    Add UI for pause menu and logic for pause menu

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,10 +1,16 @@
+using UnityEditor;
+using UnityEngine.SceneManagement; 
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PauseManager : MonoBehaviour
 {
     #region Variables
 
-    private bool _isPaused; 
+    private bool _isPaused;
+    [SerializeField] public Button ExitButton;
+    [SerializeField] public Button ResumeButton;
+    [SerializeField] public GameObject PauseView;
 
     #endregion
 
@@ -13,10 +19,17 @@ public class PauseManager : MonoBehaviour
 
     private void Update()
     {
-       if (Input.GetKeyDown(KeyCode.Escape))
-            TogglePause();
+        if (Input.GetKeyDown(KeyCode.Escape))
+
+            if (_isPaused)
+            {
+                TogglePause();
+            }
+            else
+            {
+                Resume();
+            }
     }
-    
 
     #endregion
 
@@ -25,8 +38,28 @@ public class PauseManager : MonoBehaviour
 
     private void TogglePause()
     {
+        PauseView.SetActive(true);
         _isPaused = !_isPaused;
-        Time.timeScale = _isPaused ? 0 : 1; 
+        Time.timeScale = 0;
+    }
+    
+    #endregion
+
+
+    #region Public methods
+
+    public void Resume()
+    {
+        PauseView.SetActive(false);
+        _isPaused = !_isPaused;
+        Time.timeScale = 1;
+    }
+    
+    public void ApplicationQuit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
     }
 
     #endregion
