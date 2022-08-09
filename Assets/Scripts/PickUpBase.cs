dc539862b5acdb04072556f810a331e5e532dd6d
DanilaAneichik <danila.aneychik2005@gmail.com>
2022-08-09 16:41:13 +0300

    Add PickUp Base logic.

---
+++ Assets/Scripts/PickUpBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class PickUpBase : MonoBehaviour
+{
+    #region Unity lifecycle
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        ApplyEffect(col);
+    }
+
+
+    #region Protected regions
+
+    protected abstract void ApplyEffect(Collision2D col);
+
+    #endregion
+
+    #endregion
+}
